b2ba906648978d4cfd7379f67e0f6a014f088e28
Sabnaaa1 <112567839+Sabnaaa1@users.noreply.github.com>
2023-02-20 16:55:58 +0700

    Add files via upload

---
+++ 2257051014_ MEITA AYU SABNA DAMAYANTI_INTRO.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main (){
+ string a;
+ cout<<"Nama Lengkap : MEITA AYU SABNA DAMAYANTI ";
+ getline (cin,a);
+ 
+ string b;
+ cout<<"Kelas : ILKOM A ";
+ getline(cin,b);
+ 
+ string c;
+ cout<<"Input Nilai DDP : MANTAP ";
+ getline (cin,c);  
+ 
+ 	cout<<"--------------------------------"<<endl;
+    cout<<a<<endl<<b<<endl<<c;
+    
+ return 0;
+}
---
+++ 2257051014_MEITA AYU SABNA DAMAYANTI_SUHU.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+	float c, r, f, k;
+	cin>>c;
+	r=c*4/5;
+	f= (c*9/5)+32;
+	k=c+273;
+	cout<<"Celcius : "<<c<<endl<<"Reamur : "<<r<<endl<<"Fahrenhait : "<<f<<endl<<"Kelvin :"<<k;
+	return 0;
+}
